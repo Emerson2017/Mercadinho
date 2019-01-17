a8248401b83d915af4c1a4fa89be07d262518f78
emerson2017 <emersondima@hotmail.com>
2019-01-17 08:54:11 -0300

    Data access object da entidaide produto

---
+++ src/dao/ProdutoDao.java
@@ -0,0 +1,79 @@
+package dao;
+
+import model.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import dao.ConnectionFactory;
+
+public class ProdutoDao {
+
+	Connection conexao;
+	
+	public ProdutoDao(){
+		this.conexao = new ConnectionFactory().getConnection();
+	}
+	
+	
+	public void Adiciona(Produto produto) {
+		String sql = "INSERT INTO produto (descricao, preco) values (?,?)";
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, produto.getDescricao());
+			stmt.setDouble(2, produto.getPreco());
+			stmt.execute();
+		}catch (Exception erro) {
+			throw new RuntimeException("Erro ao tentar inserir produto " + erro.getMessage());
+		}finally {
+			try {
+				stmt.close();
+			}catch (Exception erro) {
+				throw new RuntimeException("Erro ao tentar fechar conexao: "+erro.getMessage());
+			}
+		}
+	}
+	
+	public void Atualiza(Produto produto) {
+		String sql = "UPDATE produto SET descricao = ?, preco = ? WHERE codigo_produto = ?";
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, produto.getDescricao());
+			stmt.setDouble(2, produto.getPreco());
+			stmt.setInt(3, produto.getCodigo_produto());
+			stmt.execute();
+		}catch (Exception erro) {
+			throw new RuntimeException("Erro ao tentar atualizar produto " + erro.getMessage());
+		}finally {
+			try {
+				stmt.close();
+			}catch (Exception erro) {
+				throw new RuntimeException("Erro ao tentar fechar conexao: "+erro.getMessage());
+			}
+		}
+	}
+	
+	public void Atualiza(Produto produto) {
+		String sql = "UPDATE produto SET descricao = ?, preco = ? WHERE codigo_produto = ?";
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, produto.getDescricao());
+			stmt.setDouble(2, produto.getPreco());
+			stmt.setInt(3, produto.getCodigo_produto());
+			stmt.execute();
+		}catch (Exception erro) {
+			throw new RuntimeException("Erro ao tentar atualizar produto " + erro.getMessage());
+		}finally {
+			try {
+				stmt.close();
+			}catch (Exception erro) {
+				throw new RuntimeException("Erro ao tentar fechar conexao: "+erro.getMessage());
+			}
+		}
+	}
+	
+}
