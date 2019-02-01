26ab801e27572a43934f19edff9a7d57327e457b
emerson2017 <emersondima@hotmail.com>
2019-02-01 01:12:04 -0300

    refatorando classe de conexao

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -3,13 +3,15 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConnectionFactory {
-	public Connection getConnection() {	
+//classe de conexao ao banco de dados
+public class ConnectionFactory{
+	public Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://localhost/mercado", "root", "1q2w3e4r");
-		}catch (Exception error) {
-			throw new RuntimeException("Erro ao estabelecer conexao com o banco: " + error.getMessage());
+		}catch(Exception e) {
+			throw new RuntimeException("Erro ao tentar se conectar com o banco de dados: "+e.getMessage()); 
 		}
+
 	}
 }
